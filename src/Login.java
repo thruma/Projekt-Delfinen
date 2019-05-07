4b21b9bb9583030849af00a9dd56424726b2e56a
Birgith christiansen <birgithchristiansen@hotmail.com>
2019-05-07 10:27:02 +0200

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,39 @@
-public class Login {
+import java.util.*;
 
-    // Login for de 3 forskellige brugere af programmet
-    // Formand, træner, restance-person(?)
-}
+public class Login {
+    public void login (List<Motionist> motionister, List<KonkurrenceMedlem> konkurrencemedlemmer) {
+        System.out.println("Velkommen til Delfin Systemet!");
+        System.out.println("Indtast dit kodeord for at fortsætte:");
+        Menu menu = new Menu();
+
+
+
+        Scanner scLogin = new Scanner(System.in);
+        String loginValg = "0";
+
+
+        if (loginValg == null) {
+            return loginValg;
+        }
+        switch (loginValg) {
+            case "Formand":
+                //Kald på F Menu
+                menu.fMenu(motionister, konkurrencemedlemmer);
+                break;
+            case "Træner":
+                //Kald på T menu
+                menu.tMenu();
+
+                break;
+            case "Kasser":
+                //Kald på K menu
+                menu.kMenu();
+                break;
+            default:
+                System.out.println("Det indtastede er ugyldigt. Prøv igen.");
+                break;
+            //TO DO: Loop - så default leder tilbage til, at man kan taste koden.
+            //TO DO: Man har kun 3 forsøg til at skrive koden.
+        }//slut på switch*/
+    }//slut på metoden
+}//slut på Login klassen
